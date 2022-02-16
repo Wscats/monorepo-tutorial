efcbfde4dd54b5cc86cf068af5a135fe3b19c77e
enoyao <enoyao@tencent.com>
2022-02-16 20:28:21 +0800

    demo

--- test/cli.js
+++ test/cli.js
@@ -1,5 +1,6 @@
 const child_process = require("child_process");
 const path = require("path");
+const fs = require("fs");
 const args = process.argv.slice(2);
 const [command, ...commandArgs] = args;
 
@@ -12,6 +13,22 @@ switch (command) {
         // child: test ↓
         // project = child; target = test
         const [project, target] = commandArgs[0].split(':');
+        const packageJson = fs.readFileSync(path.join(__dirname, `${project}/package.json`), 'utf-8').toString();
+        const projectJson = fs.readFileSync(path.join(__dirname, `${project}/project.json`), 'utf-8').toString();
+        const { scripts } = JSON.parse(packageJson);
+        const { targets } = JSON.parse(projectJson);
+        // 融合 project.json(nx) 和 package.json(npm) 的 scripts 命令
+        let mergePackageAndProjectScripts = {};
+        Object.keys(scripts || {}).forEach((script) => {
+            mergePackageAndProjectScripts[script] = {
+                // 默认的执行器
+                executor: '@nrwl/workspace:run-script',
+                options: {
+                    script,
+                },
+            };
+        });
+        mergePackageAndProjectScripts = { ...mergePackageAndProjectScripts, ...(targets || {}) };
         child_process.execSync({
             install: 'npm install',
             add: 'npm install',
